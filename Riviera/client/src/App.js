5c3cbd24163a3ade928b75d69cac3338c3c9103d
diorithaxhiolli <diorithaxhiolli1@gmail.com>
2025-03-16 01:46:20 +0100

    Design tweaks

--- Riviera/client/src/App.js
+++ Riviera/client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/home/Home';
+import Reservations from './components/reservations/Reservations';
 import Header from './components/nav/Header';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/reservations" element={<Reservations />} />
       </Routes>
     </Router>
   );
--- Riviera/client/src/components/home/Home.js
+++ Riviera/client/src/components/home/Home.js
@@ -12,20 +12,7 @@ const Home = () => {
                 <button className="reserve-btn">Reserve</button>
             </div>
 
-            <div className="content-boxes">
-                <div className="content-box">
-                    <img src="/images/example1.jpg" alt="Dish 1" className="box-image" />
-                    <p className="box-text">Authentic Italian Pasta</p>
-                </div>
-                <div className="content-box">
-                    <img src="/images/example4.jpg" alt="Dish 2" className="box-image" />
-                    <p className="box-text">Wood-Fired Pizza</p>
-                </div>
-                <div className="content-box">
-                    <img src="/images/example3.jpg" alt="Dish 3" className="box-image" />
-                    <p className="box-text">Fresh Mediterranean Salad</p>
-                </div>
-            </div>
+         
         </div>
     );
 };
